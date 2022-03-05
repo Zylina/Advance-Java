0eeb96c826b6ffd85e866bf34df6323f9b4f9636
Zylina <guptahimanshi002@gmail.com>
2022-03-05 13:55:28 +0530

    "Spring Handsome"

---
+++ Spring-Handsone/src/main/java/com/org/demo/EmployeeDAO.java
@@ -0,0 +1,5 @@
+package com.org.demo;
+
+public interface EmployeeDAO {
+	  public String getEmployeeName(int id);
+	}
---
+++ Spring-Handsone/src/main/java/com/org/demo/SpringJDBCConfiguration.java
@@ -0,0 +1,38 @@
+package com.org.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class SpringJDBCConfiguration {
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    //MySQL database we are using
+    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/login");//change url
+    dataSource.setUsername("root");//change userid
+    dataSource.setPassword("root@_123");//change pwd
+    return dataSource;
+  }
+
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    jdbcTemplate.setDataSource(dataSource());
+    return jdbcTemplate;
+  }
+
+  @Bean
+  public EmployeeDAO employeeDAO(){
+    EmployeeDAOImpl empDao = new EmployeeDAOImpl();
+    empDao.setJdbcTemplate(jdbcTemplate());
+    return empDao;
+  }
+
+}
+
